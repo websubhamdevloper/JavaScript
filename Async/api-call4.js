e8f7335ff79403415fbfb3eb884e42d582642c00
websubhamdevloper <156600798+websubhamdevloper@users.noreply.github.com>
2026-03-21 22:09:54 +0530

    video making

--- Async/api-call4.js
+++ Async/api-call4.js
@@ -1,2 +1,13 @@
-// New API call 
-// oops 
+async function API() {
+    const api = await fetch("https://randomuser.me/api/");
+    const data = await api.json();
+
+    const [ user ] = data.results;
+    const {name, gender, phone, email} = user;
+
+    console.log(name.first, name.last)
+    console.log(gender)
+    console.log(phone)
+    console.log(email)
+}
+API();
